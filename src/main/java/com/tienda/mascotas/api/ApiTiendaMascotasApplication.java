dac76157df3d5734c189ab4f53ed3ac2dc176988
MorenoNegrito <negrito.pankeke@gmailcom>
2025-11-16 02:16:05 -0300

    aa

--- src/main/java/com/tienda/mascotas/api/ApiTiendaMascotasApplication.java
+++ src/main/java/com/tienda/mascotas/api/ApiTiendaMascotasApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiTiendaMascotasApplication {
 
+    //a
+    //aa
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTiendaMascotasApplication.class, args);
         System.out.println("🐾 PetShop Backend iniciado exitosamente! 🐾");
