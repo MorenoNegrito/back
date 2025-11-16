0ef49b0a393eeef33585b8a672fb61005cff62fa
MorenoNegrito <negrito.pankeke@gmailcom>
2025-11-16 10:26:10 -0300

    fix: Optimizar conexión Supabase y agregar CORS

--- src/main/java/com/tienda/mascotas/api/ApiTiendaMascotasApplication.java
+++ src/main/java/com/tienda/mascotas/api/ApiTiendaMascotasApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiTiendaMascotasApplication {
-
+//a
     //aa
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTiendaMascotasApplication.class, args);
