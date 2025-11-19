09eeda25b149c6aeee4c5173afb5709692a699aa
MorenoNegrito <negrito.pankeke@gmailcom>
2025-11-19 09:27:02 -0300

    Implementacion final swagger??

---
+++ src/main/java/com/tienda/mascotas/api/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.tienda.mascotas.api.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Tienda de Mascotas")
+                        .version("1.0.0")
+                        .description("Documentación de la API REST para la gestión de productos, usuarios y pedidos en Tienda de Mascotas.")
+                        .contact(new Contact()
+                                .name("Joaquin Iturriaga")
+                                .email("tuemail@ejemplo.com")
+                                .url("https://github.com/tuusuario"))
+                );
+    }
+}
